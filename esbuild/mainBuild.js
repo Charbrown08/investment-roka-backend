877e5a6a86cb2378dea0dc0db2dde964926eb9aa
Charbrown08 <carlos.moreno.jimenez@konecta-group.com>
2024-04-18 15:17:45 -0500

    [ADD] compiler and package

---
+++ esbuild/mainBuild.js
@@ -0,0 +1,28 @@
+const esbuild = require('esbuild')
+const { findFiles } = require('./filterBaseLambdas')
+
+const baseDirectories = ['src/functions']
+const entryPoints = findFiles(baseDirectories, '.js')
+
+const baseOptions = {
+  entryPoints: entryPoints,
+  entryNames: '[dir]/[name]',
+  bundle: true,
+  platform: 'node',
+  target: 'node18',
+  outdir: 'dist',
+  outbase: 'src'
+}
+
+const buildOptions = {
+  main: {
+    ...baseOptions,
+    minify: true
+  },
+  offline: {
+    ...baseOptions,
+    keepNames: true
+  }
+}
+
+esbuild.build(buildOptions[process.argv[2]]).catch(() => process.exit(1))
